3355447a8da3415d6b40294dedec294b94e11cd4
Bao Ly <lyngocgiabao1958@gmail.com>
2025-01-28 18:45:52 +1100

    Use TPU for eigen worm dataset

--- utils.py
+++ utils.py
@@ -71,7 +71,12 @@ def create_dirs(dirs):
 def Initialization(config):
     if config['seed'] is not None:
         torch.manual_seed(config['seed'])
-    device = torch.device('cuda' if (torch.cuda.is_available() and config['gpu'] != '-1') else 'cpu')
+    device = torch.device('cuda' if (torch.cuda.is_available() and config['gpu'] != '-1') else 'mps')
+    import torch_xla
+    import torch_xla.core.xla_model as xm
+    
+    device = xm.xla_device()
+    torch.set_default_tensor_type('torch.FloatTensor')
     logger.info("Using device: {}".format(device))
     if device == 'cuda':
         logger.info("Device index: {}".format(torch.cuda.current_device()))
